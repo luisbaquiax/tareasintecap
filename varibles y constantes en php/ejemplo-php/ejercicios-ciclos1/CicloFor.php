7ae19d558826ab7d8f9b5c883746a76a52e551b8
luisbaquiax1995 <luisbaquiax201430801@cunoc.edu.gt>
2024-09-18 10:11:54 -0600

    update: tablas multiplicar del 3

--- varibles y constantes en php/ejemplo-php/ejercicios-ciclos1/CicloFor.php
+++ varibles y constantes en php/ejemplo-php/ejercicios-ciclos1/CicloFor.php
@@ -19,12 +19,9 @@ echo '<h2>Bienvenido 3 veces</h2>';
 for ($i = 0; $i < 3; $i++) {
     echo ($i + 1) . '. Bienvenido   ';
 }
-echo '<h2>Tabla de multiplicar del 3</h2>';
+echo '<h2>Tablas de multiplicar del 3</h2>';
 for ($j = 1; $j < 11; $j++) {
-    echo 'Tablas de multiplicar del ' . $j . '<br>';
-    for ($k = 1; $k < 11; $k++) {
-        echo $j.' x '. $k . ' = '.($j * $k).'<br>';
-   }
+    echo '3 x '. $j . ' = '.(3 * $j).'<br>';
 }
 ?>
 </body>
